dd3951054198285baa33ddd2e6724f4ba61c38b2
Georgii Ustinov <ustinovgo@gmail.com>
2024-10-27 20:50:00 +0200

    [fix] Invalid package names for lua files

---
+++ config/nvim/lua/texvide/config.lua
@@ -0,0 +1,17 @@
+-- General settings
+vim.opt.number = true
+vim.opt.autoindent = true
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.mouse = 'a'
+vim.opt.swapfile = false
+vim.opt.shiftwidth = 2
+vim.opt.whichwrap:append('<,>,h,l,[,]')
+
+vim.opt.spell = true
+vim.opt.spelllang = { 'en', 'ru' }
+
+
+vim.g.auto_save = 1
+vim.g.perl_host_prog = '/usr/bin/perl'
---
+++ config/nvim/lua/texvide/plugins.lua
@@ -0,0 +1,7 @@
+-- Vim plug
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+    Plug('lervag/vimtex') -- Vim
+    Plug('SirVer/ultisnips') -- Snippets
+    Plug('907th/vim-auto-save') -- Auto save
+vim.call('plug#end')
