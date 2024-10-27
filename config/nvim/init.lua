6b7d0a518682ed33618c6b0635702e81e4bf7d5b
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-10-27 20:17:52 +0200

    Deprecate coc-snippets

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,23 +15,23 @@ vim.opt.spelllang = { 'en', 'ru' }
 -- Vim plug
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
--- VimTex
-Plug('lervag/vimtex') 
--- CocSnippets
-Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
+Plug('lervag/vimtex') -- Vim
+-- Plug('neoclide/coc.nvim', { ['branch'] = 'release' }) -- CocSnippets
+Plug('SirVer/ultisnips') -- Snippets
+Plug('907th/vim-auto-save') -- Auto save
 vim.call('plug#end')
 
 
 -- Function to check for backspace
-local function check_backspace()
-  local col = vim.fn.col('.') - 1
-  return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
+-- local function check_backspace()
+--   local col = vim.fn.col('.') - 1
+--   return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+-- end
 
 -- Mappings
-vim.api.nvim_set_keymap('i', '<Tab>', [[coc#pum#visible() ? coc#pum#next(1) : v:lua.check_backspace() ? "\<Tab>" : coc#refresh()]], { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap('i', '<S-Tab>', [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], { noremap = true, silent = true, expr = true })
-vim.api.nvim_set_keymap('i', '<CR>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], { noremap = true, silent = true, expr = true })
+-- vim.api.nvim_set_keymap('i', '<Tab>', [[coc#pum#visible() ? coc#pum#next(1) : v:lua.check_backspace() ? "\<Tab>" : coc#refresh()]], { noremap = true, silent = true, expr = true })
+-- vim.api.nvim_set_keymap('i', '<S-Tab>', [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], { noremap = true, silent = true, expr = true })
+-- vim.api.nvim_set_keymap('i', '<CR>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], { noremap = true, silent = true, expr = true })
 
 -- VimTeX configuration
 vim.g.tex_flavor = 'latex'
@@ -41,16 +41,13 @@ vim.g.vimtex_quickfix_mode = 0
 vim.opt.conceallevel = 1
 vim.g.tex_conceal = 'abdmg'
 
-vim.g.auto_save = 0 -- Disable AutoSave
+-- Auto save configuration
+vim.g.auto_save = 1
 
 -- UltiSnips configuration
-vim.g.UltiSnipsExpandTrigger = '<c-j>'
-vim.g.UltiSnipsJumpForwardTrigger = '<c-s>'
-vim.g.UltiSnipsJumpBackwardTrigger = '<c-z>'
+vim.g.UltiSnipsExpandTrigger = '<c-l>'
+vim.g.UltiSnipsJumpForwardTrigger = '<c-j>'
+vim.g.UltiSnipsJumpBackwardTrigger = '<c-k>'
 vim.g.UltiSnipsSnippetDirectories = { os.getenv('HOME') .. '/.config/nvim/UltiSnips' }
-
--- Coc-Snippets
-vim.g.coc_snippet_next = '<tab>'
-
 
 vim.g.perl_host_prog = '/usr/bin/perl'
