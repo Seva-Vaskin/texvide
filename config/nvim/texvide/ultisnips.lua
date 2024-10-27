fc888aee9864cfb2102ecf9717b461ef07344851
Georgii Ustinov <ustinovgo@gmail.com>
2024-10-27 21:13:01 +0200

    [fix] remove duplicate configs

--- config/nvim/texvide/ultisnips.lua
+++
@@ -1,5 +0,0 @@
--- UltiSnips configuration
-vim.g.UltiSnipsExpandTrigger = '<c-l>'
-vim.g.UltiSnipsJumpForwardTrigger = '<c-j>'
-vim.g.UltiSnipsJumpBackwardTrigger = '<c-k>'
-vim.g.UltiSnipsSnippetDirectories = { os.getenv('HOME') .. '/.config/nvim/UltiSnips' }
