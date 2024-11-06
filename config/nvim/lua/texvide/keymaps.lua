a2c61ef22f634d0999308df5921763affa1f32d7
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-11-06 19:37:50 +0200

    Add undo/redo/quit hotkeys

--- config/nvim/lua/texvide/keymaps.lua
+++ config/nvim/lua/texvide/keymaps.lua
@@ -1,7 +1,20 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true }
 
+-- Window movement
 keymap('n', '<c-j>', '<c-w>j', opts)
 keymap('n', '<c-h>', '<c-w>h', opts)
 keymap('n', '<c-k>', '<c-w>k', opts)
 keymap('n', '<c-l>', '<c-w>l', opts)
+-- Undo
+keymap('n', '<C-z>', 'u', opts)
+keymap('v', '<C-z>', 'u', opts)
+keymap('i', '<C-z>', '<C-o>u', opts)
+-- Redo
+keymap('n', '<C-S-z>', '<C-r>', opts)
+keymap('v', '<C-S-z>', '<C-r>', opts)
+keymap('i', '<C-S-z>', '<C-o><C-r>', opts)
+-- Quit
+keymap('n', '<C-q>', ':q<CR>', opts)
+keymap('v', '<C-q>', ':q<CR>', opts)
+keymap('i', '<C-q>', '<C-o>:q<CR>', opts)
