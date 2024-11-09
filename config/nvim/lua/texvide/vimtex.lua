48c990892971467303a71556e5ab6276e419b5c2
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-11-09 15:39:50 +0200

    Minor improvements

--- config/nvim/lua/texvide/vimtex.lua
+++ config/nvim/lua/texvide/vimtex.lua
@@ -1,7 +1,7 @@
 -- VimTeX configuration
 vim.g.tex_flavor = 'latex'
 vim.g.vimtex_view_method = 'zathura'
-vim.g.vimtex_view_zathura_options = '--mode fullscreen'
+vim.g.vimtex_view_zathura_options = os.getenv('HOST_OS') == 'Darwin' and '--mode fullscreen' or ''
 vim.g.vimtex_compiler_progname = 'nvr'
 vim.g.vimtex_quickfix_mode = 0
 vim.opt.conceallevel = 1
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ python3 /inkscape-shortcut-manager/main.py >>/dev/null 2>&1 &
 # service dbus start
 # export $(dbus-launch)
 
-echo "Current Docker DPI: ${DPI}"
-xpra start :80 --dpi=${DPI} --bind-tcp=0.0.0.0:8080 --tray=no
+xpra start :80 --bind-tcp=0.0.0.0:8080 --tray=no
 
 exec bash
