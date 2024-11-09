815e0486f096f8616a2166f798a06d849680b827
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-11-09 14:11:32 +0200

    Open zathura as stand alone window

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ python3 /inkscape-shortcut-manager/main.py >>/dev/null 2>&1 &
 # service dbus start
 # export $(dbus-launch)
 
-xpra start :80 --bind-tcp=0.0.0.0:8080
+echo "Current Docker DPI: ${DPI}"
+xpra start :80 --dpi=${DPI} --bind-tcp=0.0.0.0:8080 --tray=no
 
 exec bash
---
+++ launch_xrpa.py
@@ -0,0 +1,3 @@
+import webview
+webview.create_window("Zathura", "http://localhost:8080/?floating_menu=no")
+webview.start()
